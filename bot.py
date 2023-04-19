1de614b8e18951308e8b14827ca4ac89fc8d609e
Tarallo <98182571+Tarall0@users.noreply.github.com>
2023-04-19 10:37:00 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,13 +1,18 @@
 import discord
 import responses
-from discord.ext import commands
+import random
+from discord.ext.commands import Bot, bot
+from discord.ui import View, Button
 
-description = '''TEST DESC'''
+TOKEN = 'Token'
+black_words = ["http://", "https://", "www."]
+offensive_words = []
 
-TOKEN = ''
-black_words = ["http://", "https://"]
+intents = discord.Intents.default()
+intents.message_content = True
+client = discord.Client(intents=intents)
 
-
+bot = Bot(command_prefix="!", intents=intents.all())
 
 
 async def send_message(message, user_message, is_private):
@@ -19,20 +24,19 @@ async def send_message(message, user_message, is_private):
 
 
 def run_discord_bot():
-    intents = discord.Intents.default()
-    intents.message_content = True
-    client = discord.Client(intents=intents)
-
-
-
-
     @client.event
     async def on_ready():
         print(f'{client.user} is now running as BOT')
 
+    @client.event
+    async def on_member_join(self, member, message):
+        guild = member.guild
+        if guild.system_channel is not None:
+            to_send = f'Welcome {member.mention} to {guild.name}!'
+            await message.channel.send(to_send)
 
     @client.event
-    async def on_message(message):
+    async def on_message(message, text=None):
         # if message.author != client.user
         if message.author == client.user:
             return
@@ -41,17 +45,60 @@ def run_discord_bot():
         user_message = str(message.content)
         channel = str(message.channel)
 
-        for text in black_words:
-            if "Key Role" not in str(message.author.roles) and text in str(message.content.lower()):
-                print(f"Moderated message > Deleted - Member: {message.author}")
-                await message.delete()
-                await message.channel.send(f"I just deleted {message.author.display_name}'s last message because it is not allowed")
-                return
+        for text in white_words:
+            pass
+            print("Checked")
+        else:
+            for text in black_words:
+                if "Key Role" not in str(message.author.roles) and text in str(message.content.lower()):
+                    print(f"Moderated message > Deleted - Member: {message.author}")
+                    await message.delete()
+                    await message.author.send(f"Hey {message.author.display_name}! 👋")
+                    await message.author.send(
+                        f"Ho rimosso il tuo ultimo messaggio, perchè contiene del testo non ammesso. 😔")
+                    await message.channel.send(
+                        f"Ho appena rimosso il messaggio di {message.author.display_name} perchè pare contenga contenuto non ammesso")
+                    return
 
-        print("Moderated message > Ok")
+            print("Moderated message > Ok")
+
+        if "Limited Access" not in str(message.author.roles):
+            if user_message == '!tod':
+                view = View()
+                button = Button(label="Verità", style=discord.ButtonStyle.grey, emoji="🔥")
+                button2 = Button(label="Obbligo", style=discord.ButtonStyle.grey, emoji="💀")
+                view.add_item(button)
+                view.add_item(button2)
+
+                embed = discord.Embed(title="Obbligo o verità?",
+                                      description='Genera un obbligo o una verità, casuale!')
+                await message.channel.send(embed=embed, view=view)
+
+                interaction = await bot.wait_for("button_click", check=lambda i: i.custom_id == "button")
+                await interaction.send(content="Button clicked!")
+
+        if user_message == "!roll":
+            roll = str(random.randint(1, 6))
+            embed = discord.Embed(title=":game_die: Dice roll",
+                                  description=f"{username} just rolled a dice \r \r Result: **{roll}**")
+            await message.channel.send(embed=embed)
+
+        if user_message == "!roll 20":
+            roll = str(random.randint(1, 20))
+            embed = discord.Embed(title=":game_die: Dice roll - D20",
+                                  description=f"{username} just rolled a dice \r \r Result: **{roll}**")
+            await message.channel.send(embed=embed)
+
+        if user_message == "!help":
+            view = View()
+            button_link = Button(label="stonedzone.it", url="https://stonedzone.it", style=discord.ButtonStyle.url, emoji="")
+            view.add_item(button_link)
+            embed = discord.Embed(title=":heart: MushApp",
+                                  description=f"This python bot-application is just here for testing and learning purposes \r \r **Commands available** \r \r **!roll** - Roll a dice (d6) \r **!roll 20** - Roll a dice  (d20) \r **!tod** - Generate a truth or a dare \r **!quote** - Get a random quote from an array\r \r \r `Currently under developement an auto-moderating system for texting channel, try typing 'https://'`")
+            await message.channel.send(embed=embed, view=view)
 
         if message.content.lower().startswith("bot"):
-            await message.channel.send(f"Hi, {message.author.display_name}")
+            await message.channel.send(f"Ciao, {message.author.mention}")
             await message.channel.send("What can I do for you? Digit !help")
 
         if user_message[0] == '?':
