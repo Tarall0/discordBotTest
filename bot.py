7c58a6f9460ba3ef0e85fb5c5bdac22f5579144b
Tarallo <98182571+Tarall0@users.noreply.github.com>
2023-04-20 11:13:46 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -36,7 +36,7 @@ def run_discord_bot():
             await message.channel.send(to_send)
 
     @client.event
-    async def on_message(message, text=None):
+    async def on_message(message):
         # if message.author != client.user
         if message.author == client.user:
             return
@@ -55,27 +55,24 @@ def run_discord_bot():
                     await message.delete()
                     await message.author.send(f"Hey {message.author.display_name}! 👋")
                     await message.author.send(
-                        f"Ho rimosso il tuo ultimo messaggio, perchè contiene del testo non ammesso. 😔")
+                        f"I just removed your last message because it is not allowed to entry that tipe of text 😔")
                     await message.channel.send(
-                        f"Ho appena rimosso il messaggio di {message.author.display_name} perchè pare contenga contenuto non ammesso")
+                        f"I just deleted {message.author.display_name}'s last message because it is not allowed")
                     return
 
             print("Moderated message > Ok")
 
-        if "Limited Access" not in str(message.author.roles):
-            if user_message == '!tod':
+        if "Verified ✅" not in str(message.author.roles):
+            if user_message == '!verify':
                 view = View()
-                button = Button(label="Verità", style=discord.ButtonStyle.grey, emoji="🔥")
-                button2 = Button(label="Obbligo", style=discord.ButtonStyle.grey, emoji="💀")
+                button = Button(label="Verify", style=discord.ButtonStyle.green, emoji="✅")
+                button2 = Button(label="Exit", style=discord.ButtonStyle.danger, emoji="💀")
                 view.add_item(button)
                 view.add_item(button2)
 
-                embed = discord.Embed(title="Obbligo o verità?",
-                                      description='Genera un obbligo o una verità, casuale!')
+                embed = discord.Embed(title="Verify your account",
+                                      description='Click Verify in order to get access to all channels')
                 await message.channel.send(embed=embed, view=view)
-
-                interaction = await bot.wait_for("button_click", check=lambda i: i.custom_id == "button")
-                await interaction.send(content="Button clicked!")
 
         if user_message == "!roll":
             roll = str(random.randint(1, 6))
@@ -90,15 +87,33 @@ def run_discord_bot():
             await message.channel.send(embed=embed)
 
         if user_message == "!help":
-            view = View()
-            button_link = Button(label="stonedzone.it", url="https://stonedzone.it", style=discord.ButtonStyle.url, emoji="")
-            view.add_item(button_link)
-            embed = discord.Embed(title=":heart: MushApp",
-                                  description=f"This python bot-application is just here for testing and learning purposes \r \r **Commands available** \r \r **!roll** - Roll a dice (d6) \r **!roll 20** - Roll a dice  (d20) \r **!tod** - Generate a truth or a dare \r **!quote** - Get a random quote from an array\r \r \r `Currently under developement an auto-moderating system for texting channel, try typing 'https://'`")
-            await message.channel.send(embed=embed, view=view)
+            embed = discord.Embed(title="ℹ️ MushApp",
+                                  description=f"This python bot-application is just here for testing and learning purposes \r \r **Commands available** \r \r **!roll** - Roll a dice (d6) \r **!roll 20** - Roll a dice  (d20) \r **!verify** - Required in order to obtain access to all channels \r **!quote** - Get a random quote \r \r \r `Currently under developement an auto-moderating system for texting channel, try typing 'https://'`")
+            await message.channel.send(embed=embed)
+
+        if user_message == "420" or user_message == "!420":
+
+            gif_list = ["https://media.tenor.com/dl""-5x95WKh8AAAAC/420.gif",
+                        "https://media.tenor.com/Qm1qCnh4_TEAAAAS/420-the-simpsons.gif",
+                        "https://media.tenor.com/wZTOpYp88VwAAAAS/ah-smoke.gif",
+                        "https://media.tenor.com/dl""-5x95WKh8AAAAC/420.gif",
+                        "https://media.tenor.com/A6MkSkO-WiEAAAAS/weed-time-weed.gif",
+                        "https://media.tenor.com/yGEuK0ItsWsAAAAS/weed-blunt.gif"
+                        ]
+            random_var = random.randint(0, 5)
+            random_gif = gif_list[random_var]
+
+            respons_ft = [f"Felice 420, {message.author.mention}!",
+                          f"Hell yeah! {message.author.mention} #420",
+                          f"Oggi 04/20 2023, accendila {message.author.mention}"]
+
+            i = random.randint(0, 2)
+            random_resp = respons_ft[i]
+            await message.channel.send(random_resp)
+            await message.channel.send(random_gif)
 
         if message.content.lower().startswith("bot"):
-            await message.channel.send(f"Ciao, {message.author.mention}")
+            await message.channel.send(f"Hi, {message.author.mention}")
             await message.channel.send("What can I do for you? Digit !help")
 
         if user_message[0] == '?':
